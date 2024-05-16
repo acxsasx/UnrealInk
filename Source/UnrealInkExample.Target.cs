b1a908144b754d10f83eed135baf706300e32688
Adam C Thompson <adamc_thompson@yahoo.com>
2024-05-16 12:49:02 -0400

    5.0 changes

--- Source/UnrealInkExample.Target.cs
+++ Source/UnrealInkExample.Target.cs
@@ -8,7 +8,7 @@ public class UnrealInkExampleTarget : TargetRules
 	public UnrealInkExampleTarget(TargetInfo Target) : base(Target)
 	{
 		Type = TargetType.Game;
-		DefaultBuildSettings = BuildSettingsVersion.V2;
+		DefaultBuildSettings = BuildSettingsVersion.Latest;
 
 		ExtraModuleNames.AddRange( new string[] { "UnrealInkExample" } );
 	}
